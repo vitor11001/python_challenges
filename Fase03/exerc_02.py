1323675edbf46e6d324a0f98bc5857a9abb105c3
bmariano305 <bruno.mariano@senfio.com>
2023-01-23 14:55:57 -0300

    Execicios da fase 3 add

---
+++ Fase03/exerc_02.py
@@ -0,0 +1,8 @@
+""" 
+Olá, este é o um exercício da fase 03. 
+
+Crie as funções a seguir:
+    - Crie uma função que recebe uma lista de idades e retorna, numa tupla, somente a maior idade.
+    - Crie uma função que recebe uma lista de idades e retorna um dicionário com as chaves "MAIOR_IDADE" E "MENOR_IDADE".
+    - Crie uma função que recebe uma lista de idades e retorna uma tupla com as idade ordenadas de maneira crescente.
+"""
---
+++ Fase03/exerc_03.py
@@ -0,0 +1,22 @@
+""" 
+Olá, este é o um exercício da fase 03.
+
+Crie uma função que recebe um dicionário com os nomes dos alunos reprovados e aprovados em cada matéria.
+(exemplo de entrada)
+{
+    "MATEMÁTICA": {"APROVADOS": [A,B,C], "REPROVADOS": [D,E,F,G,H]},
+    "FÍSICA": {"APROVADOS": [A,B,C], "REPROVADOS": [D,E,F,G,H]}, 
+    ...
+}
+Após receber a entrada, retorne as chaves "APROVADOS_EM_TODAS" com os alunos numa tupla ordenada 
+alfabeticamente, a chave "REPROVADOS" com uma tupla também ordenada alfabeticamente e por fim uma
+chave "DETALHES" com um dicionário que mostra os alunos reprovados ordenados pela quantidade de 
+matérias reprovadas. 
+(exemplo da chave detalhes)
+{
+    1: {"nome":[ matérias, reprovadas,...]},
+    2: {"nome":[ matérias, reprovadas,...]},
+    ...
+}
+
+"""
