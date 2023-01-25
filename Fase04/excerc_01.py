d539baa57f7a1cec180a861e01af4b236b671f6b
José Vitor <83318136+vitor11001@users.noreply.github.com>
2023-01-25 17:18:25 -0300

    Add files via upload

--- Fase04/excerc_01.py
+++ Fase04/excerc_01.py
@@ -9,5 +9,47 @@ Exercícios:
 
 """
 
-path_arquivo_A = ""
-path_arquivo_B = ""
+class Fase_4():
+    def __init__(self, arquivo: str):
+        self.arquivo = arquivo
+        self.senfio = 'Senfio'
+        
+    def confere_existencia(self):
+        try:
+            with open(self.arquivo, 'r') as arq:
+                return True
+            
+        except FileNotFoundError:
+            print(f'Erro, arquivo não encontrado, confira o diretorio: {self.arquivo}')
+            return False
+            
+    def abre_arquivo_adiciona(self):
+        try:
+            with open(self.arquivo, 'a') as arq:
+                print(f'Arquivo aberto! {self.arquivo}')
+                arq.write('\n' + self.senfio)
+        
+        except FileNotFoundError:
+            print(f'Erro, arquivo não encontrado, confira o diretorio: {self.arquivo}')
+
+    def abre_cria_edita_arquivo(self):
+        try:
+            with open(self.arquivo, 'w+') as arq:
+                print(f'Arquivo aberto! {self.arquivo}')
+                arq.write(self.senfio)
+        
+        except FileNotFoundError:
+            print(f'Erro, arquivo não encontrado, confira o diretorio: {self.arquivo}')
+            
+            
+def main():
+    path_arquivo_A = "./Fase04/arquivo_A.txt"
+    path_arquivo_B = "./Fase04/arquivo_B.txt"
+    
+    if Fase_4(path_arquivo_A).confere_existencia() == True:
+        Fase_4(path_arquivo_A).abre_arquivo_adiciona()
+        
+    Fase_4(path_arquivo_B).abre_cria_edita_arquivo()
+
+if __name__ == '__main__':
+    main()
